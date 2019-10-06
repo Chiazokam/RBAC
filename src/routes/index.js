2994d0ad4bbeb6953eab57021ac93ff0232961ce
Chiazokam Echeta <chiazokamecheta@gmail.com>
2019-10-06 23:51:33 +0100

    story(category) create categories endpoints
    - create categories endpoints
    - define permissions
    
    [Delivers]

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRoute from './auth.routes';
 import userRoute from './user.routes';
 import productRoute from './product.routes';
+import categoryRoute from './category.routes';
 
 const route = new Router();
 
 route.use('/auth', authRoute);
 route.use('/', userRoute);
 route.use('/', productRoute);
+route.use('/', categoryRoute);
 
 export default route;
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 import { Router } from 'express';
 import {
-  getProducts, updateProduct, deleteProduct,
+  getProducts, updateProduct, deleteProduct, createProduct,
 } from '../controllers/product.controllers';
 import { isUserSignedIn, grantAccess } from '../middlewares/auth.middlewares';
 
 const route = new Router();
 
-route.get('/products', isUserSignedIn, grantAccess('readAny', 'product'), getProducts);
+route.post('/products', isUserSignedIn, grantAccess('createAny', 'product'), createProduct);
+route.get('/products', getProducts);
 route.patch('/products/:id', isUserSignedIn, grantAccess('updateAny', 'product'), updateProduct);
 route.delete('/products/:id', isUserSignedIn, grantAccess('deleteAny', 'product'), deleteProduct);
 
